9e100af5a3393f7c13723e72a67de83165104d2f
KyleVNiek <kylevn185@gmail.com>
2021-02-24 10:35:59 +0200

    Second comment

--- prg381.java
+++ prg381.java
@@ -5,7 +5,7 @@ PRG381
     public static void main(String[] args) {
         System.out.println("First Java file on Git");
         //comment test
-        // a different comment
+        // another different comment
     }
     
 }   
